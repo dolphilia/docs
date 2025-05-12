b9a40c71060e5bc52a3466adaf18c8eb538b755d
dolphilia <dolphilia@outlook.com>
2025-05-12 17:22:33 +0900

    update

---
+++ docs/sample-docs/assets/content-modules.Bx6Ap1S1.js
@@ -0,0 +1,16 @@
+const contentModules = new Map([
+["src/content/docs/en/v1/guide/extended-icons-example.mdx", () => import('./extended-icons-example.Bd5i2Fc6.js')],
+["src/content/docs/en/v1/guide/getting-started.mdx", () => import('./getting-started.CduL_E-g.js')],
+["src/content/docs/en/v1/guide/icons-example.mdx", () => import('./icons-example.CoAwmDqq.js')],
+["src/content/docs/en/v1/guide/tabs-example.mdx", () => import('./tabs-example.Ddp91vqW.js')],
+["src/content/docs/en/v1/guide/installation.mdx", () => import('./installation.Ce5MyaJ_.js')],
+["src/content/docs/ja/v1/guide/extended-icons-example.mdx", () => import('./extended-icons-example.BHYJg1NW.js')],
+["src/content/docs/en/v1/guide/sidebar-auto-generation.mdx", () => import('./sidebar-auto-generation.LSAohlsE.js')],
+["src/content/docs/ja/v1/guide/sidebar-auto-generation.mdx", () => import('./sidebar-auto-generation.DRzxVcRB.js')],
+["src/content/docs/ja/v1/guide/getting-started.mdx", () => import('./getting-started.vCGTTsOt.js')],
+["src/content/docs/ja/v1/guide/icons-example.mdx", () => import('./icons-example.BIBp-8BD.js')],
+["src/content/docs/ja/v1/guide/tabs-example.mdx", () => import('./tabs-example.DAWfShFC.js')],
+["src/content/docs/ja/v2/guide/getting-started.mdx", () => import('./getting-started.DoU9MMC-.js')],
+["src/content/docs/en/v2/guide/getting-started.mdx", () => import('./getting-started.zajZ_qel.js')]]);
+
+export { contentModules as default };
